230c7448945fe488ea963221b65f41e9257e85dd
Furkan Kaan Alkan <alkanfurkankaan@gmail.com>
2026-06-27 23:38:43 +0300

    synchronize static assets and make 'read more' button functional

--- psySite/psySite/settings.py
+++ psySite/psySite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'psySite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,4 +116,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
